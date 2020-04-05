a1dfa141eb307cf43dc7cca5c3e6da0007a31833
Pablo Satler <pablosatler@gmail.com>
2020-04-05 14:07:48 -0300

    chore: move main entry file into the src folder

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.safeArea}>
+        <Text> Hello, World! </Text>
+        <Text> Hello, World! </Text>
+        <Text> Hello, World! </Text>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  safeArea: {
+    // justifyContent: 'center',
+    // alignItems: 'center',
+  },
+});
+
+export default App;
