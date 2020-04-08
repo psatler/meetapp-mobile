8d9d6dd8066359e578bd48701c92543d2200a598
Pablo Satler <pablosatler@gmail.com>
2020-04-07 22:27:48 -0300

    feat: create login/auth flow with some public and protected routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
-const AppStack = createStackNavigator();
-
-// screens
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import Routes from './routes';
 
 function App() {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator>
-        <AppStack.Screen
-          options={{ headerShown: false }}
-          component={SignIn}
-          name="SignIn"
-        />
-        <AppStack.Screen component={SignUp} name="SignUp" />
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
+  return <Routes />;
 }
 
 export default App;
