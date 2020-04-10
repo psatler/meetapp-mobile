1d17933acaa557438f453b7e5375ca517f5334c0
Pablo Satler <pablosatler@gmail.com>
2020-04-10 20:58:25 -0300

    feat: add status bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import Routes from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#ffafbd" />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
